857b556d3003de10c7bb762b30b5b6a4e7de63f3
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-10-09 17:36:59 +0200

    minor update

--- stack-functions-azure/terraform/module-function/storage_account.tf
+++ stack-functions-azure/terraform/module-function/storage_account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name = "cycloid${var.cy_project}${var.cy_env}"
+  name = replace("cycloid${var.cy_project}${var.cy_env}", "-", "")
   resource_group_name = local.resource_group_name
   location = local.resource_group_location
   account_kind = "Storage"
