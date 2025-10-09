2bc05f22b4e5f0c8e2c0a23e8c00382cc9da5128
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-10-09 12:15:29 +0200

    minor update

--- stack-functions-azure/terraform/variables.tf
+++ stack-functions-azure/terraform/variables.tf
@@ -1,7 +1,8 @@
 # Cycloid variables
-variable "env" {}
-variable "project" {}
-variable "customer" {}
+variable "cy_org" {}
+variable "cy_project" {}
+variable "cy_env" {}
+variable "cy_component" {}
 
 # Cloud variables
 variable "azure_cred" {
