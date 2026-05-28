0aab3cb845087163547bf1a885ee3fe2377bcbd6
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-05-28 12:55:35 +0200

    Update AWS credential handling in pipeline configurations and variable definitions to streamline access and improve documentation.

--- stack-network-aws/terraform/variables.tf
+++ stack-network-aws/terraform/variables.tf
@@ -6,7 +6,11 @@ variable "cy_component" {}
 
 # AWS variables
 variable "aws_cred" {
-  description = "Contains AWS access_key and secret_key"
+  description = "AWS credentials from the environment cloud account (access_key, secret_key)."
+  type = object({
+    access_key = string
+    secret_key = string
+  })
 }
 variable "aws_region" {
   description = "AWS region where to create servers (from environment env_vars)."
