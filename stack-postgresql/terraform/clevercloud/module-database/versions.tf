c9cd393c647d92831fdf12373b147927e6c11164
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2026-06-21 17:12:39 +0200

    Add CleverCloud provider to Terraform configuration for database module
    - Introduced the CleverCloud provider with version constraints to ensure compatibility.
    - This update enhances the module's integration with CleverCloud's infrastructure management capabilities.

--- stack-postgresql/terraform/clevercloud/module-database/versions.tf
+++ stack-postgresql/terraform/clevercloud/module-database/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    clevercloud = {
+      source  = "CleverCloud/clevercloud"
+      version = "~> 2.0"
+    }
     cycloid = {
       source  = "cycloidio/cycloid"
       version = ">= 0.7.3"
